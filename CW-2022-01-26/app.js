178b4853697ebfdcad9d055850057cccbf542ac0
patelnk4 <patelnk4@vcu.edu>
2022-01-26 21:51:26 -0600

    Codewars

---
+++ CW-2022-01-26/app.js
@@ -0,0 +1,10 @@
+// Grasshopper - Variable Assignment Debug
+
+// DESCRIPTION:
+// Variable assignment
+// Fix the variables assigments so that this code stores the string 'devLab' in the variable name.
+
+var a = "dev"
+var b = "Lab"
+
+var name = a + b
