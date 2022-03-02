70d28b2f0a6d14a3bb4a40090ce49840483e4065
patelnk4 <patelnk4@vcu.edu>
2022-03-01 22:02:37 -0600

    Codewars

---
+++ CW-2022-02-29/app.js
@@ -0,0 +1,9 @@
+// Function 2 - squaring an argument
+// DESCRIPTION:
+// Now you have to write a function that takes an argument and returns the square of it.
+
+
+function square(n)
+{
+  return n*n
+}
