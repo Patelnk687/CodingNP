5f0ca2c428fa178f537ba0fc5c228cecde112ece
patelnk4 <patelnk4@vcu.edu>
2022-08-29 21:07:13 -0500

    Codewars

---
+++ CW-2022-08-29/app.js
@@ -0,0 +1,10 @@
+// Reversed Strings
+// Complete the solution so that it reverses the string passed into it.
+
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+
+function solution(str){
+    var stnd = str.split("").reverse().join("")
+    return stnd
+      }
