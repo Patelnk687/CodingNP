e3ff468f678b918d0f6d89fd5ef29d0b99f227a1
Patelnk687 <patelnk687@gmail.com>
2022-12-10 12:07:21 -0600

    Code Problem

---
+++ CW-2022-12-10/app.js
@@ -0,0 +1,12 @@
+// let arrray=[2,10,5,6,7]
+// console.log(arrray.sort());
+// console.log(arrray.sort(function(a,b){return a-b}));
+
+
+let newArray=[78,52,89,55,23,2,1,3]
+
+console.log(newArray.sort())
+
+console.log(newArray.sort(function(a,b){return a-b}));
+
+console.log(newArray.sort(function(a,b){return b-a}))
